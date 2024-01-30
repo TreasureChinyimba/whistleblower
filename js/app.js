afcbbf830cceb3b0a9fbc21361bab1c2decabab5
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-01-30 22:47:23 +0200

    creating a script to show visibility on hover

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const whyFraud = document.getElementById('fraud')
+const dropDown = document.querySelector('.drop');
+
+whyFraud.addEventListener('mouseover', ()=>{
+    alert('works')
+})
+
+console.log(whyFraud);
