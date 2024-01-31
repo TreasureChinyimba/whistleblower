d1707b4f3b0ab40a2bda86f1be5741646c13c3c6
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-01-31 19:52:30 +0200

    hover drop down successfull

--- js/app.js
+++ js/app.js
@@ -7,3 +7,18 @@ fraud.addEventListener('mouseover', ()=>{
     dropDown.style= 'visibility: visible;';
     
 })
+
+dropDown.addEventListener('mouseover', ()=>{
+    dropDown.style= 'visibility: visible;';
+    
+})
+
+dropDown.addEventListener('mouseleave', ()=>{
+    dropDown.style= 'visibility: hidden;';
+    
+})
+
+fraud.addEventListener('mouseleave', ()=>{
+    dropDown.style= 'visibility: hidden;';
+    
+})
